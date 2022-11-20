2f1b4abb1e83800fe235f2ef71dfd03cc035f1f3
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-20 17:59:30 +0300

    render json data

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    get "/cheese", to: "cheese#index"
+  end
+
 end
