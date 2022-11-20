fbd540d5ca1458cc74d3439bfe4184926943e841
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-20 23:30:04 +0300

    create teacher resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :teachers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   Rails.application.routes.draw do
+  resources :teachers
     get "/cheese", to: "cheese#index"
   end
 
