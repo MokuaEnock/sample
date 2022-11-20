a39f8edd37d9ae43c9283f6639a1af46f9d14c87
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-20 23:53:53 +0300

    add column and update schema

--- db/migrate/20221120204858_add_sex_to_students.rb
+++ db/migrate/20221120204858_add_sex_to_students.rb
@@ -1,4 +1,6 @@
 class AddSexToStudents < ActiveRecord::Migration[7.0]
   def change
+    add_column :students, :sex, :string
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_20_203709) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_20_204858) do
   create_table "schools", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_20_203709) do
     t.integer "class"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sex"
   end
 
   create_table "teachers", force: :cascade do |t|
