e3cab03beff84d6680055ba0defd1f1cf1609101
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-20 23:22:45 +0300

    create model generator

--- app/controllers/cheese_controller.rb
+++ app/controllers/cheese_controller.rb
@@ -1,5 +1,5 @@
 class CheeseController < ApplicationController
   def index
-    render json: {hello: "Cheese World"}
+    render json: { hello: "Cheese World", mokua: "Kijana Maridadi" }
   end
 end
